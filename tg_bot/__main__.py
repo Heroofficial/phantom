5433f1c6c9fbaf12dd5964de3cea79c7f84116da
Hero Gamer <62762419+Heroofficial@users.noreply.github.com>
2020-09-11 18:19:11 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -154,7 +154,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add Shizuka to your group", url="t.me/{}?startgroup=true".format(bot.username))],
-                                                                                  [InlineKeyboardButton(text="❓ Help", url="https://t.me/EmiliaHikariBot?start=help")]]))
+                                                                                  [InlineKeyboardButton(text="❓ Help", url="https://t.me/Shizu_Bot?start=help")]]))
 
 
     else:
